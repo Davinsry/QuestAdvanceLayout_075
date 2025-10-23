0d1c8e68f303629f4ece0fdee48936a7aa90d1c5
Davinsry <davinipad53@gmail.com>
2025-10-23 11:22:59 +0700

    Pemnambahan data di strings.XML

---
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -0,0 +1,10 @@
+package com.example.myuipertama
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun AktvitasPertama(modifier: Modifier){
+    Column { modifier = Modifier }
+}
