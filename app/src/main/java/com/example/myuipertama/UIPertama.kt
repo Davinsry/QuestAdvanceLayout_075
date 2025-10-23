30ec06edca768b6ea089702f5e37617a86fa190e
Davinsry <davinipad53@gmail.com>
2025-10-23 12:04:49 +0700

    menambahkan fontSize = 30.sp,                                     fontFamily = FontFamily.Cursive,                                     color = Color.White,                                     modifier = Modifier.padding(top = 15.dp)                                     pada Text

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -17,8 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,8 +54,10 @@ fun AktvitasPertama(modifier: Modifier){
                 Column() {
                     Text(
                         stringResource(R.string.nama),
-
-
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
                     )
                 }
             }
